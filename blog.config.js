2d8786e5689d7825162cc424152ff9132326b075
garygreengu <140234367+garygreengu@users.noreply.github.com>
2023-07-27 15:14:39 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -20,7 +20,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '特看中国AI', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '中国AI博主，全观全球AI动态，体察中国AI变化，细品AI场景应用，服务人民AI生活。欢迎来到我的博客，感谢你的关注', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://chineseai.link', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '中国AI, 博客，全观全球AI动态，体察中国AI变化，细品AI场景应用，服务人民AI生活', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '中国AI, 博客，全观全球AI动态，体察中国AI变化，细品AI场景应用，服务人民AI生活,AI应用，AI教育，AI资源，AI市场，AI生活', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '3450771135@qq.com', // 邮箱地址 例如mail@tangly1024.com
   CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || '', // 你的微博个人主页
